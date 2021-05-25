2b4bd71b4bed9a4b0f889ddb8bed97991a1c2fd4
Shane Drabing <shane.drabing@gmail.com>
2021-05-25 14:07:59 -0600

    versioning

--- polyfoto.py
+++ polyfoto.py
@@ -1,3 +1,8 @@
+__author__ = "Shane Drabing"
+__license__ = "MIT"
+__version__ = "0.0.1"
+__email__ = "shane.drabing@gmail.com"
+
 import os
 import sys
 
---
+++ setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="polyfoto",
+    version="0.0.1",
+    author="Shane Drabing",
+    author_email="shane.drabing@gmail.com",
+    packages=setuptools.find_packages(),
+    url="https://github.com/shanedrabing/polyfoto",
+    description="Create image mosaics.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+    ],
+    data_files=[
+        ("", ["LICENSE.txt"])
+    ],
+    install_requires=[
+        "cv2", "imageio", "numpy"
+    ]
+)
