02d5ce1b0c72a26997fab90ff2ecbddf1c163650
Shane Drabing <shane.drabing@gmail.com>
2021-06-21 11:07:07 -0600

    new version

--- polyfoto.py
+++ polyfoto.py
@@ -1,6 +1,6 @@
 __author__ = "Shane Drabing"
 __license__ = "MIT"
-__version__ = "0.2.1"
+__version__ = "0.2.2"
 __email__ = "shane.drabing@gmail.com"
 
 import os
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setuptools.setup(
     name="polyfoto",
-    version="0.2.1",
+    version="0.2.2",
     author="Shane Drabing",
     author_email="shane.drabing@gmail.com",
     packages=setuptools.find_packages(),
